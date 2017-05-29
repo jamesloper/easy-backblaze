552e2110b5584f4efa19abdaa05fd8c33961bd52
James <james@hux.com>
2017-05-29 18:38:03 -0400

    keep init for one minute

--- index.js
+++ index.js
@@ -1,45 +1,47 @@
 var path = require('path');
 var fs = require('fs');
-var async = require('async');
 var backend = require('./rawbackend.js');
 
 class B2 {
-	constructor(user, password) {
-		this.auth = {'user': user, 'password': password};
+	constructor(user, password, config = {}) {
+		this._auth = {'user': user, 'password': password};
+		this._config = Object.assign({connections: 4, bucket: null}, config);
+		this._init = {expires:0};
 	}
 	
-	init(callback) {		
-		backend.authorizeAccount(this.auth, (err, settings) => {
+	init(callback) {
+		// If initted in the last minute don't bother initing again
+		if (Date.now() < this._init.expires) 
+			return callback(null, this._init);
+		
+		backend.authorizeAccount(this._auth, (err, settings) => {
 			if (err) return callback(err);
 
 			backend.listBuckets(settings, (err, data) => {
 				if (err) return callback(err);
-				if (data.buckets.length == 0) return callback(new Error(404, 'No buckets in B2 account!'));
-
-				callback(null, {buckets:data.buckets, settings:settings});
+				if (data.buckets.length == 0) return callback(new Error(404, 'no buckets in account'));
+				
+				this._init = {'buckets': data.buckets, 'settings': settings, 'expires': Date.now() + 60000};
+				callback(null, this._init);
 			});
 		});
 	}
 	
-	uploadFile(options, callback) {
-		if (!options instanceof Object) 
-			throw new Error(500, '<options> needs to ba an object: {*file:Buffer, bucket:String, name:String}');
-		if (!callback || !callback instanceof Function) 
-			throw new Error(500, 'No callback was passed');
-		if (!options.file || !options.file instanceof Buffer) 
-			throw new Error(500, '<options.file> needs to be a buffer!');
-		if (options.name && !options.name instanceof String)
-			throw new Error(500, '<options.name> must be a string!');
-		if (options.bucket && !options.bucket) 
-			throw new Error(404, '<options.bucket> must be a bucketId or the name of a bucket!');
+	uploadFile(options, callback) {		
+		if (!options instanceof Object) throw new Error(400, 'options must be an object');
+		if (!callback || !callback instanceof Function) throw new Error(400, 'no callback was passed');
+		if (!options.file || !options.file instanceof String) throw new Error(400, 'file needs to be a string');
+		if (options.name && !options.name instanceof String) throw new Error(400, 'name must be a string');
+		if (options.bucket && !options.bucket instanceof String) throw new Error(400, 'bucket must be a string');
 		
 		this.init((err, context) => {
 			if (err) return callback(err);
 			
 			let bucket = context.buckets[0];
-			if (options.bucket) {
-				bucket = context.buckets.find(r => r.bucketName == options.bucket);
-				if (!bucket) throw new Error(404, `Bucket with name "${options.bucket}" could not be found`);
+			let preferredBucket = options.bucket || this._config.bucket;
+			if (preferredBucket) {
+				bucket = context.buckets.find(r => r.bucketName == preferredBucket);
+				if (!bucket) throw new Error(404, `bucket ${options.bucket} could not be found`);
 			}
 
 			fs.readFile(options.file, (err, fileBuffer) => {
